a4c7a94300e0b52ef2c7e98e133374b0d640889e
Judhunja <judxhunja@gmail.com>
2024-08-07 17:51:19 +0300

    use express.json to parse json request body

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || '5000';
 
 const routes = require('./routes/index');
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use('/', routes);
 
