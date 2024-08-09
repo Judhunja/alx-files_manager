929e4b9130de32b0b3625d20f36fa41d7ad65774
Judhunja <judxhunja@gmail.com>
2024-08-09 21:34:37 +0300

    catch error from invalid Base64 string

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -14,27 +14,30 @@ export const getConnect = async (req, resp) => {
 
   const base64 = authHeader.split(' ')[1];
 
-  const toString = Buffer.from(base64, 'base64').toString('ascii');
-  const [email, password] = toString.split(':');
+  try {
+    const toString = Buffer.from(base64, 'base64').toString('ascii');
+    const [email, password] = toString.split(':');
+    const users = dbClient.db.collection('users');
+    const sha1pwd = crypto.createHash('sha1').update(password).digest('hex');
+    const user = await users.findOne({ email, password: sha1pwd });
 
-  const users = dbClient.db.collection('users');
-  const sha1pwd = crypto.createHash('sha1').update(password).digest('hex');
-  const user = await users.findOne({ email, password: sha1pwd });
+    if (!user) {
+      return resp.status(401).json({ error: 'Unauthorized' });
+    }
 
-  if (!user) {
+    const tkn = uuidv4();
+
+    const key = `auth_${tkn}`;
+
+    redisClient.client.set(key, user._id.toString(), 'EX', 86400, (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+    return resp.status(200).json({ token: tkn });
+  } catch (err) {
     return resp.status(401).json({ error: 'Unauthorized' });
   }
-
-  const tkn = uuidv4();
-
-  const key = `auth_${tkn}`;
-
-  redisClient.client.set(key, user._id.toString(), 'EX', 86400, (err) => {
-    if (err) {
-      console.error(err);
-    }
-  });
-  return resp.status(200).json({ token: tkn });
 };
 
 export const getDisconnect = async (req, resp) => {
